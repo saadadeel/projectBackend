cad85839d1dfbb564674de8382dad50d1ee5a884
Saad Adeel <saadadeel@Saads-MacBook-Pro.local>
2016-02-29 19:30:31 +0000

    Made changes to Run and races

--- app/models/UserSocketActor.java
+++ app/models/UserSocketActor.java
@@ -5,6 +5,7 @@ import com.mongodb.DB;
 import com.mongodb.MongoClient;
 import org.jongo.Jongo;
 import org.jongo.MongoCollection;
+import play.api.mvc.Result;
 import play.libs.Json;
 
 /**
@@ -26,16 +27,13 @@ public class UserSocketActor extends UntypedActor {
     public void onReceive(Object message) throws Exception {
         if (message instanceof String) {
 
-            DB db = new MongoClient("178.62.68.172", 27017).getDB("competifitDB");
-            Jongo jongo = new Jongo(db);
-            MongoCollection users = jongo.getCollection("users");
-            user one = users.findOne("{'username':'" + message + "'}").as(user.class);
+//            DB db = new MongoClient("178.62.68.172", 27017).getDB("competifitDB");
+//            Jongo jongo = new Jongo(db);
+//            MongoCollection users = jongo.getCollection("users");
+//            user one = users.findOne("{'username':" + message + "}").as(user.class);
 
             out.tell("yoo", self());
+//            out.tell(Json.toJson(one), self());
         }
-    }
-
-    public void setOut(){
-
     }
 }
