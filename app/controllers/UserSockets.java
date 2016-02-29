e84030bb15546b35950a214add99a9d6b562c266
Saad Adeel <saadadeel@Saads-MacBook-Pro.local>
2016-02-29 16:45:23 +0000

    Made changes to Run and races

--- app/controllers/UserSockets.java
+++ app/controllers/UserSockets.java
@@ -1,8 +1,11 @@
 package controllers;
 
+import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Props;
 import models.UserSocketActor;
 import org.jetbrains.annotations.NotNull;
+import play.libs.F;
 import play.mvc.*;
 
 import play.mvc.WebSocket;
@@ -16,6 +19,11 @@ public class UserSockets extends Controller {
 
     @NotNull
     public static WebSocket getUser() {
-        return WebSocket.withActor(UserSocketActor::props);
+//        return WebSocket.withActor(UserSocketActor::props);
+        return WebSocket.withActor(new F.Function<ActorRef, Props>() {
+            public Props apply(ActorRef out) throws Throwable {
+                return UserSocketActor.props(out);
+            }
+        });
     }
 }
