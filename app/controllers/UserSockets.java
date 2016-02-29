d24b054b574c2172c2e3d0ac0c935ee9f1d5a16e
Saad Adeel <saadadeel@Saads-MacBook-Pro.local>
2016-02-29 16:51:10 +0000

    Made changes to Run and races

--- app/controllers/UserSockets.java
+++ app/controllers/UserSockets.java
@@ -20,10 +20,19 @@ public class UserSockets extends Controller {
     @NotNull
     public static WebSocket<String> getUser() {
 //        return WebSocket.withActor(UserSocketActor::props);
-        return WebSocket.withActor(new F.Function<ActorRef, Props>() {
-            public Props apply(ActorRef out) throws Throwable {
-                return UserSocketActor.props(out);
+
+//        return WebSocket.withActor(new F.Function<ActorRef, Props>() {
+//            public Props apply(ActorRef out) throws Throwable {
+//                return UserSocketActor.props(out);
+//            }
+//        });
+//    }
+
+        return new WebSocket<String>() {
+            public void onReady(WebSocket.In<String> in, WebSocket.Out<String> out) {
+                out.write("Hello!");
+                out.close();
             }
-        });
+        };
     }
 }
